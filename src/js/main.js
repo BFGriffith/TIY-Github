ce89e38dc9b8efd875f876be2661a9d1b5e6e441
Epicurean306 <benjaminforbesgriffith@gmail.com>
2016-03-05 16:37:26 -0500

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,27 @@
-(function(window){
+(function(window) {
 
-  $('li').click(function(){
-    $(this).siblings().removeClass('tab-current');
-    $(this).addClass('tab-current');
-    $('section').removeClass('content-current');
-    $($('a', this).attr('href')).addClass('content-current');
+  $.getJSON('/api/github/users/al-the-x.json')
+  /**
+   * Replacing placeholders with data from...?
+   *
+   * @param user
+   * @return ???
+   */
+  .then(function(user){
+    var nameTpl = _.template($('.profile-name').html());
+
+    console.log(nameTpl({ 'user': user }));
+
+    $('.profile-name').html(nameTpl({ 'user': user }));
   });
+})(window);
+
+
+$('li').click(function() {
+$(this).siblings().removeClass('tab-current');
+$(this).addClass('tab-current');
+$('section').removeClass('content-current');
+$($('a', this).attr('href')).addClass('content-current');
+});
 
 })(window);
